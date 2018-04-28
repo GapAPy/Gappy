d7736644689b1438e5f4827661722e85552d07d7
Sadegh Yazani <silverstar10@gmail.com>
2018-04-29 01:56:38 +0430

    change description for pypi

--- setup.py
+++ setup.py
@@ -27,7 +27,43 @@ setup(
     version=version,
 
     description='Python framework for Gap Service API',
-    long_description='Python framework for Gap Service API',
+    long_description="""### Install Package
+```
+pip3 install gappy
+```
+compatible python version: 3.4+
+
+### How to use
+
+#### Run with flask
+
+Example:
+
+```
+import gappy
+from flask import Flask
+from flask import request
+
+TOKEN = '<your token>'
+
+bot = gappy.Bot(TOKEN)
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def parse_request():
+    msg = request.form
+    content_type = msg['type']
+    chat_id = msg['chat_id']
+    data = msg['data']
+    bot.send_text(chat_id, data)
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run()
+```
+""",
 
     url='https://github.com/hsin75/Gappy',
 
